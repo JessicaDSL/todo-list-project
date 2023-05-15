2c4add2f3b29bfbf42de6d3ae751da0c53bb9fdc
Jessica Lima <jessikurt@gmail.com>
2023-05-15 20:00:38 -0300

    change directory for global style

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import GlobalStyle from './globalStyle.ts'
+import GlobalStyle from './global-style.ts'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
